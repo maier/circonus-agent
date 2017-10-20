211e75667151d3ff3831c8651115380eff0fe4c7
matt maier <maier@users.noreply.github.com>
2017-10-20 15:25:01 -0400

    initial wmi skeleton

---
+++ internal/builtins/collector/wmi/main.go
@@ -0,0 +1,44 @@
+// Copyright © 2017 Circonus, Inc. <support@circonus.com>
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+package wmi
+
+import (
+	"time"
+
+	"github.com/circonus-labs/circonus-agent/internal/builtins/collector"
+	cgm "github.com/circonus-labs/circonus-gometrics"
+)
+
+// New creates new WMI collector
+func New(cfgFile string) (collector.Collector, error) {
+	return &wmicommon{id: "not_implemented"}, collector.ErrNotImplemented
+}
+
+// Collect returns collector metrics
+func (p *wmicommon) Collect() error {
+	return collector.ErrNotImplemented
+}
+
+// Flush returns last metrics collected
+func (p *wmicommon) Flush() cgm.Metrics {
+	return cgm.Metrics{}
+}
+
+// ID returns the id of the instance
+func (p *wmicommon) ID() (string, error) {
+	return p.id, nil
+}
+
+// Inventory returns collector stats for /inventory endpoint
+func (p *wmicommon) Inventory() (collector.InventoryStats, error) {
+	return collector.InventoryStats{
+		ID:              p.id,
+		LastRunStart:    p.lastStart.Format(time.RFC3339Nano),
+		LastRunEnd:      p.lastEnd.Format(time.RFC3339Nano),
+		LastRunDuration: p.lastRunDuration.String(),
+		LastError:       p.lastError.Error(),
+	}, nil
+}
---
+++ internal/builtins/collector/wmi/vars.go
@@ -0,0 +1,30 @@
+// Copyright © 2017 Circonus, Inc. <support@circonus.com>
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+package wmi
+
+import (
+	"sync"
+	"time"
+
+	cgm "github.com/circonus-labs/circonus-gometrics"
+	"github.com/rs/zerolog"
+)
+
+// wmicommon defines WMI metrics common elements
+type wmicommon struct {
+	id                  string
+	lastEnd             time.Time
+	lastError           error
+	lastMetrics         cgm.Metrics
+	lastRunDuration     time.Duration
+	lastStart           time.Time
+	logger              zerolog.Logger
+	metricStatus        map[string]bool // list of metrics and whether they should be collected or not, may be overriden in config file
+	metricStatusDefault string          // default collection status for metrics NOT explicitly in metricStatus, may be overriden in config file
+	running             bool
+	runTTL              time.Duration // may be overriden in config file (default is for every request)
+	sync.Mutex
+}
