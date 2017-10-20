d971a0b26b4844cfbca2755a93141d023535bfd9
matt maier <maier@users.noreply.github.com>
2017-10-20 15:25:38 -0400

    initial procfs cpu

---
+++ internal/builtins/collector/procfs/cpu.go
@@ -0,0 +1,15 @@
+// Copyright © 2017 Circonus, Inc. <support@circonus.com>
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+//
+
+// +build !linux
+
+package procfs
+
+import "github.com/circonus-labs/circonus-agent/internal/builtins/collector"
+
+// NewCPUMetrics creates new procfs cpu collector
+func NewCPUMetrics(cfgFile string) (collector.Collector, error) {
+	return &pfscommon{id: "not_implemented"}, collector.ErrNotImplemented
+}
