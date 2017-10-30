4fc5ca0ad545f658b8bf18ed70d85dc98d8534b5
matt maier <maier@users.noreply.github.com>
2017-10-30 08:01:12 -0400

    return err if new fails

--- internal/builtins/configure_linux.go
+++ internal/builtins/configure_linux.go
@@ -13,6 +13,9 @@ import (
 
 func (b *Builtins) configure() error {
 	collectors, err := procfs.New()
+	if err != nil {
+		return err
+	}
 	for _, c := range collectors {
 		b.collectors[c.ID()] = c
 	}
