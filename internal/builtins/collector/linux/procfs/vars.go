f7b3f25e26b1890a52a863b8c34630cf0920e416
matt maier <maier@users.noreply.github.com>
2017-10-30 06:54:06 -0400

    comment procfs package vars

--- internal/builtins/collector/linux/procfs/vars.go
+++ internal/builtins/collector/linux/procfs/vars.go
@@ -6,6 +6,8 @@
 package procfs
 
 import (
+	"fmt"
+	"regexp"
 	"sync"
 	"time"
 
@@ -15,17 +17,30 @@ import (
 
 // pfscommon defines ProcFS metrics common elements
 type pfscommon struct {
-	file                string // may be overriden in config file
-	id                  string
-	lastEnd             time.Time
-	lastError           string
-	lastMetrics         cgm.Metrics
-	lastRunDuration     time.Duration
-	lastStart           time.Time
-	logger              zerolog.Logger
-	metricStatus        map[string]bool // list of metrics and whether they should be collected or not, may be overriden in config file
-	metricDefaultActive bool            // default status for metrics NOT explicitly in metricStatus, may be overriden in config file
-	running             bool
-	runTTL              time.Duration // may be overriden in config file (default is for every request)
+	file                string          // OPT proc file path, may be overriden in config file
+	id                  string          // OPT id of the collector (used as metric name prefix)
+	lastEnd             time.Time       // last collection end time
+	lastError           string          // last collection error
+	lastMetrics         cgm.Metrics     // last metrics collected
+	lastRunDuration     time.Duration   // last collection duration
+	lastStart           time.Time       // last collection start time
+	logger              zerolog.Logger  // collector logging instance
+	metricDefaultActive bool            // OPT default status for metrics NOT explicitly in metricStatus, may be overriden in config file
+	metricNameChar      string          // OPT character(s) used as replacement for metricNameRegex, may be overriden in config
+	metricNameRegex     *regexp.Regexp  // OPT regex for cleaning names, may be overriden in config
+	metricStatus        map[string]bool // OPT list of metrics and whether they should be collected or not, may be overriden in config file
+	running             bool            // is collector currently running
+	runTTL              time.Duration   // OPT ttl for collections, may be overriden in config file (default is for every request)
 	sync.Mutex
 }
+
+const (
+	metricNameSeparator = "`"        // character used to separate parts of metric names
+	metricStatusEnabled = "enabled"  // setting string indicating metrics should be made 'active'
+	regexPat            = `^(?:%s)$` // fmt pattern used compile include/exclude regular expressions
+)
+
+var (
+	defaultExcludeRegex = regexp.MustCompile(fmt.Sprintf(regexPat, ""))
+	defaultIncludeRegex = regexp.MustCompile(fmt.Sprintf(regexPat, ".+"))
+)
